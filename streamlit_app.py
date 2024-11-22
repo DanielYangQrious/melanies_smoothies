a55707e1a13274c1be6a8ee5cc9be1cca5b8499f
DanielYangQrious <zirui.yang@spark.co.nz>
2024-11-22 15:51:04 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
 st.title("Example Streamlit App :balloon:")
@@ -12,6 +11,9 @@ st.write(
     [docs.streamlit.io](https://docs.streamlit.io).
     """
 )
+
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The current name on your smoothie is', name_on_order)
