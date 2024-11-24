40bce722e87fa12dade2834fca60b2cd3c7db601
DanielYangQrious <zirui.yang@spark.co.nz>
2024-11-25 09:09:02 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 from snowflake.snowpark.context import get_active_session
 
-
 # Write directly to the app
 st.title("Example Streamlit App :balloon:")
 st.write(
@@ -13,9 +12,6 @@ st.write(
     [docs.streamlit.io](https://docs.streamlit.io).
     """
 )
-
-cnx = st.connection("snowflake")
-session = cnx.session()
 
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The current name on your smoothie is', name_on_order)
@@ -56,9 +52,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
     st.success('Your Smoothie is ordered', icon = "✅")
-
-import requests
-    
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response)
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
